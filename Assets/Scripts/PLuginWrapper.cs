f7ea2df40e6fce393409aa496bc41cbffeb7fd4e
SKY <akash.rm.roy@gmail.com>
2020-04-30 10:54:52 +0530

    Update PLuginWrapper.cs

--- Assets/Scripts/PLuginWrapper.cs
+++ Assets/Scripts/PLuginWrapper.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PLuginWrapper : MonoBehaviour
+public class PluginWrapper : MonoBehaviour
 {
+    WebCamTexture webcamTexture;
+    Color32[] data;
+
     // Start is called before the first frame update
     void Start()
     {
         Text textElement = GetComponent<Text>();
         var plugin = new AndroidJavaClass("com.sky5698.unityplugin.PlugInClass");
-        textElement.text = plugin.CallStatic<string>("GetTextFromPlugin", 69);
+        textElement.text = plugin.CallStatic<string>("GetTextFromPlugin", 100) + "\n" + plugin.CallStatic<string>("GetTextureDataFromPlugin", "Camera");
+
+        //webcam initialization
+        webcamTexture = new WebCamTexture();
+        webcamTexture.Play();
+
     }
 
     // Update is called once per frame
